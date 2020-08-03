c56f9988a23690b178f91d9ba9b330ac2000c6c8
wanlun <wanlunxin@gmail.com>
2020-08-03 09:39:35 +0700

    remove daftar feature

--- app/templates/front.php
+++ app/templates/front.php
@@ -1,7 +1,5 @@
 <div class="uwrapper">
 	<a class="ulink" href="user/login">Login</a>
-	<span class="uspace"></span>
-	<a class="ulink" href="user/daftar">Daftar</a>
 </div>
 
 <div class="crwapper">
